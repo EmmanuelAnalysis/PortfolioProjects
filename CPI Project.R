cedcdc8e8a3bb7b644b7a61a386aa9cb1a45760c
EmmanuelAnalysis <148790621+EmmanuelAnalysis@users.noreply.github.com>
2023-11-11 23:21:38 -0500

    Update CPI Project.R

--- CPI Project.R
+++ CPI Project.R
@@ -1,9 +1,9 @@
 
-# This code allows us to analyze CPI and CPIXFE (excluding food and energy) data from a Statistics Canada table from 2005 to 2023. #
+# This code allows us to analyze CPI and CPIX (excluding food and energy) data from a DATA table from 2005 to 2023. #
 
 View(datacpi)
 
-# Let's create a time series for CPI and CPIXFE and plot both series on a graph.
+# Let's create a time series for CPI and CPIX and plot both series on a graph.
 
 CPI <- ts(datacpi$CPI, start=c(2005, 1), end=c(2023, 7), frequency = 12)
 CPIXFE <- ts(datacpi$CPIXFE, start=c(2005, 1), end=c(2023, 7), frequency = 12)
@@ -31,9 +31,9 @@ plotly_line_plot <- add_trace(
 
 plotly_line_plot <- add_trace(
   plotly_line_plot,
-  x = time(CPIXFE),
-  y = as.vector(CPIXFE),
-  name = "CPIXFE",
+  x = time(CPIX),
+  y = as.vector(CPIX),
+  name = "CPIX",
   type = "scatter",
   mode = "lines",
   line = list(color = "red")
@@ -41,7 +41,7 @@ plotly_line_plot <- add_trace(
 
 plotly_line_plot <- layout(
   plotly_line_plot,
-  title = "Interactive Time Series Plot of CPI and CPIXFE",
+  title = "Interactive Time Series Plot of CPI and CPIX",
   xaxis = list(title = "Time"),
   yaxis = list(title = "Inflation Rate"),
   showlegend = TRUE
@@ -53,11 +53,11 @@ plotly_line_plot
 # Using CPI and CPIXFE, we can calculate the annual inflation rate in Canada since 2000 and plot this on a graph. 
 
 annual_CPI <- aggregate(CPI, nfrequency = 1, FUN = sum)
-annual_CPIXFE <- aggregate(CPIXFE, nfrequency = 1, FUN = sum)
+annual_CPIX <- aggregate(CPIXFE, nfrequency = 1, FUN = sum)
 annual_inflation_rate_CPI <- diff(CPI, lag = 12) / lag(CPI, 12) * 100
-annual_inflation_rate_CPIXFE <- diff(CPIXFE, lag = 12) / lag(CPIXFE, 12) * 100
+annual_inflation_rate_CPIX <- diff(CPIXFE, lag = 12) / lag(CPIXFE, 12) * 100
 
-ts.plot(annual_inflation_rate_CPI, annual_inflation_rate_CPIXFE, main="Inflation Rate", col=c("blue", "red"))
+ts.plot(annual_inflation_rate_CPI, annual_inflation_rate_CPIX, main="Inflation Rate", col=c("blue", "red"))
 legend("topleft", legend=c("Inflation CPI", "Inflation CPIXFE"), lty=1:1, col=c("blue", "red"),
        bty="n", cex=0.6, x.intersp=0.5, y.intersp=0.5, xpd=TRUE)
 
